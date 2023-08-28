dead0d3fb9f36c05dc5eb9cb06da9b36a4844da5
leijiaxin <jiaxin.lei@tophant@.com>
2023-08-28 15:35:58 +0800

    fps查看天剑

--- 初始化场景_FPS.js
+++ 初始化场景_FPS.js
@@ -1,7 +1,11 @@
 import * as THREE from 'three';
+import Stat from 'three/examples/jsm/libs/stats.module.js';
 
 const w = window.innerWidth;
 const h = window.innerHeight;
+
+// Stat for fps
+const stat = new Stat();
 
 // Scene
 const scene = new THREE.Scene();
@@ -17,10 +21,20 @@ camera.position.set(0, 2, 5)
 camera.lookAt(0, 0, 0)
 
 // Objects
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const cubeGroup = new THREE.Group();
+
+function creatCube() {
+    const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+    const material = new THREE.MeshBasicMaterial({ color: 0xffffff * Math.random() });
+    const cube = new THREE.Mesh(geometry, material);
+    cube.position.set((Math.random() - 0.5) * 2, (Math.random() - 0.5) * 2, (Math.random() - 0.5) * 2)
+    cubeGroup.add(cube)
+}
+
+for (let i = 0; i < 200; i++) {
+    creatCube()
+}
+scene.add(cubeGroup);
 
 // axesHelper
 const axesHelper = new THREE.AxesHelper(2);
@@ -30,6 +44,8 @@ scene.add(axesHelper);
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(w, h)
 document.body.appendChild(renderer.domElement);
+document.body.appendChild(stat.dom);
+
 renderer.render(scene, camera);
 
 // animation
@@ -37,10 +53,12 @@ const clock = new THREE.Clock();
 
 function tick() {
     const time = clock.getElapsedTime();
-    cube.position.x = Math.sin(time * 2);
-    cube.position.y = Math.cos(time * 2);
+    cubeGroup.children.forEach((item, index) => {
+        item.rotation.z = time + index
+    })
     requestAnimationFrame(tick);
     renderer.render(scene, camera);
+    stat.update()
 }
 
 tick();
